a80612aea0d875ec58d49685143390d4e736d0b5
samdmarshall <me@samdmarshall.com>
2014-02-24 12:36:13 -0500

    adding disassembly to x86_64

--- machodiff/compare.c
+++ machodiff/compare.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <dispatch/dispatch.h>
+#include <inttypes.h>
 
 // SDM: this will give some variation due to the approximation in unique when parsing dynamically created block_ref symbols in a binary.
 void SDMDiffAddSymbols(struct loader_diff *diff, struct loader_binary *input_one, struct loader_binary *input_two) {
@@ -42,11 +43,33 @@ void SDMDiffAddSymbols(struct loader_diff *diff, struct loader_binary *input_one
 		if (unnamed_subroutine) {
 			int has_offset = sscanf(subroutine_name, kSubName, &offset);
 			if (has_offset == 1) {
-				//printf("offset: %lx %lx\n",offset,(input_one->map->subroutine_map->subroutine[index].offset - (SDMBinaryIs64Bit(input_one->header) ? (uint64_t)input_one->header : 0)));
+				printf("offset: %lx %lx\n",offset,(input_one->map->subroutine_map->subroutine[index].offset - (SDMBinaryIs64Bit(input_one->header) ? (uint64_t)input_one->header : 0)));
 			}
 		} else {
-			//printf("%s\n",subroutine_name);
+			printf("%s\n",subroutine_name);
 		}
+		
+		CoreRange subroutine_range = SDMSTRangeOfSubroutine(&(input_one->map->subroutine_map->subroutine[index]), input_one);
+		
+		csh handle;
+		cs_insn *insn;
+		size_t count;
+		
+		if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) == CS_ERR_OK) {
+			count = cs_disasm_ex(handle, Ptr(subroutine_range.offset), subroutine_range.length, offset, 0, &insn);
+			if (count > 0) {
+				size_t j;
+				for (j = 0; j < count; j++) {
+					printf("0x%"PRIx64":\t%s\t\t%s\n", insn[j].address, insn[j].mnemonic,insn[j].op_str);
+				}
+				cs_free(insn, count);
+			}
+		}
+		else {
+			printf("ERROR: Failed to disassemble given code!\n");
+		}
+		
+		cs_close(handle);
 		
 	}
 }
