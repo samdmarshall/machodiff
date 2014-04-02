9c3516cbdd9c71e47244e17d81822b1d37a8ded8
samdmarshall <me@samdmarshall.com>
2014-04-01 21:37:33 -0400

    updating capstone to build only for 64bit

--- capstone-build.sh
+++ capstone-build.sh
@@ -6,7 +6,7 @@ if [ "${ACTION}" = "clean" ]; then
 	echo "Cleaning Libraries..."
 	rm -fd "${BUILD_DIR}/Universal/*"
 	rm -fd ${CONFIGURATION_BUILD_DIR}/libcapstone-64.a
-	rm -fd ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a
+#rm -fd ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a
 	cd ./${PROJECT_NAME}
 	make clean
 fi
@@ -16,23 +16,23 @@ if [ "${ACTION}" = "build" ] || [ "${ACTION}" = "" ]; then
 
 	mkdir -p ${CONFIGURATION_BUILD_DIR}
 
-	if [ ! -f ${CONFIGURATION_BUILD_DIR}/libcapstone-64.a ]; then
+	if [ ! -f ${CONFIGURATION_BUILD_DIR}/libcapstone.a ]; then
 		echo "Building capstone for x86_64"
 		./make.sh
-		mv libcapstone.a ${CONFIGURATION_BUILD_DIR}/libcapstone-64.a
+		mv libcapstone.a ${CONFIGURATION_BUILD_DIR}/libcapstone.a
 		make clean
 	fi
 
-	if [ ! -f ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a ]; then
-		echo "Building capstone for i386"
-		./make.sh nix32
-		mv libcapstone.a ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a
-		make clean
-	fi
+#	if [ ! -f ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a ]; then
+#		echo "Building capstone for i386"
+#		./make.sh nix32
+#		mv libcapstone.a ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a
+#		make clean
+#	fi
 
-	echo "Creating fat binary"
-	cd ${BUILD_DIR}/Universal
-	lipo -create ${CONFIGURATION_BUILD_DIR}/libcapstone-64.a ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a -o libcapstone.a
+#	echo "Creating fat binary"
+#	cd ${BUILD_DIR}/Universal
+#	lipo -create ${CONFIGURATION_BUILD_DIR}/libcapstone-64.a ${CONFIGURATION_BUILD_DIR}/libcapstone-32.a -o libcapstone.a
 
 	echo "Complete"
 fi
